47d814d5422d2414dfd42a4efe3259058f5fe114
smin9408 <stmdaks888@naver.com>
2022-04-02 23:51:36 +0900

    MainActivity BaseActivity 상속

--- app/src/main/java/com/example/win_the_lotto/MainActivity.kt
+++ app/src/main/java/com/example/win_the_lotto/MainActivity.kt
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.win_the_lotto.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
     }
 }
